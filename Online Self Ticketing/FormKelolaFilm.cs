cbde3e19faef5fe7b3c39d967957fbe2a64d9b36
devano2068 <devano27921@gmail.com>
2026-04-15 14:03:16 +0700

    12

--- Online Self Ticketing/FormKelolaFilm.cs
+++ Online Self Ticketing/FormKelolaFilm.cs
@@ -19,7 +19,6 @@ namespace Online_Self_Ticketing
         {
             InitializeComponent();
         }
-
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
