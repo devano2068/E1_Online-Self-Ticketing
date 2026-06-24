1b7870cdc9f234f0b770309a7f605592975b0927
devano2068 <devano27921@gmail.com>
2026-06-24 18:01:29 +0700

    // Konfigurasi koneksi database lokal

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
-namespace Online_Self_Ticketing
+namespace Online_Self_Ticketing // Form Utama untuk Login User dan Koneksi Database
 {
     public partial class Login : Form
     {
