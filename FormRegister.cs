8a76518fd2c9063363cdf7e690df89564bea3b22
devano2068 <devano27921@gmail.com>
2026-06-24 18:07:35 +0700

    pengguna baru registrasi

--- FormRegister.cs
+++ FormRegister.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 
 namespace Online_Self_Ticketing
 {
-    public partial class FormRegister : Form
+    public partial class FormRegister : Form // pengguna baru registrasi
     {
         SqlConnection conn = new SqlConnection(
 @"Data Source=LAPTOP-BUHABIQL;Initial Catalog=BioskopDB;User ID=sa;Password=vano7474;TrustServerCertificate=True");
