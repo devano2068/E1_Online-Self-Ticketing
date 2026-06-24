ec538fcc7e47e1b613138d38ba761a0bc235eeff
devano2068 <devano27921@gmail.com>
2026-06-24 18:08:02 +0700

    pilih tiket

--- FormTiket.cs
+++ FormTiket.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 
 namespace Online_Self_Ticketing
 {
-    public partial class FormTiket : Form
+    public partial class FormTiket : Form // pilih tiket
     {
         SqlConnection conn = new SqlConnection(
             @"Data Source=LAPTOP-BUHABIQL;Initial Catalog=BioskopDB;User ID=sa;Password=vano7474;TrustServerCertificate=True");
