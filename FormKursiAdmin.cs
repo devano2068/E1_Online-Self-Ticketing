cb1608c387269d13358a26efa5f2b46f55575941
devano2068 <devano27921@gmail.com>
2026-06-25 13:42:58 +0700

    9

--- FormKursiAdmin.cs
+++ FormKursiAdmin.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 
 namespace Online_Self_Ticketing
 {
-    public partial class FormKursiAdmin : Form // tambah kursi
+    public partial class FormKursiAdmin : Form // 9
     {
         SqlConnection conn = new SqlConnection(
 @"Data Source=192.168.110.102;Initial Catalog=BioskopDB;User ID=sa;Password=vano7474");
